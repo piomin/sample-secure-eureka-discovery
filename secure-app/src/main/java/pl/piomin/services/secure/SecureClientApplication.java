d7618aa5edd455754047a8a6cf0d6361cff42fad
piotr.minkowski <piotr.minkowski@gmail.com>
2018-05-21 10:16:50 +0200

    security settings

--- secure-app/src/main/java/pl/piomin/services/secure/SecureClientApplication.java
+++ secure-app/src/main/java/pl/piomin/services/secure/SecureClientApplication.java
@@ -19,12 +19,12 @@ public class SecureClientApplication {
 	@Bean
 	public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws NoSuchAlgorithmException {
 		DiscoveryClient.DiscoveryClientOptionalArgs args = new DiscoveryClient.DiscoveryClientOptionalArgs();
-		System.setProperty("javax.net.ssl.keyStore", "src/main/resources/account.jks");
+		System.setProperty("javax.net.ssl.keyStore", "src/main/resources/client.jks");
 		System.setProperty("javax.net.ssl.keyStorePassword", "123456");
-		System.setProperty("javax.net.ssl.trustStore", "src/main/resources/account.jks");
+		System.setProperty("javax.net.ssl.trustStore", "src/main/resources/client.jks");
 		System.setProperty("javax.net.ssl.trustStorePassword", "123456");
 		EurekaJerseyClientBuilder builder = new EurekaJerseyClientBuilder();
-		builder.withClientName("account-client");
+		builder.withClientName("client");
 		builder.withSystemSSLConfiguration();
 		builder.withMaxTotalConnections(10);
 		builder.withMaxConnectionsPerHost(10);
