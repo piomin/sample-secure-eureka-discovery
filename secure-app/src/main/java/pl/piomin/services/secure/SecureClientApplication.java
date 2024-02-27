4b3abd9dd7e5e27aa9a5ca0e6000eaa5874c20e3
piomin <piotr.minkowski@gmail.com>
2024-02-28 00:15:15 +0100

    fix ssl/tls settings (2)

--- secure-app/src/main/java/pl/piomin/services/secure/SecureClientApplication.java
+++ secure-app/src/main/java/pl/piomin/services/secure/SecureClientApplication.java
@@ -10,24 +10,4 @@ public class SecureClientApplication {
         new SpringApplicationBuilder(SecureClientApplication.class).run(args);
     }
 
-//    @Bean
-//    public RestTemplateDiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws NoSuchAlgorithmException {
-//        RestTemplateDiscoveryClientOptionalArgs args = new RestTemplateDiscoveryClientOptionalArgs();
-//
-//        System.setProperty("javax.net.ssl.keyStore", "src/main/resources/client.jks");
-//        System.setProperty("javax.net.ssl.keyStorePassword", "123456");
-//        System.setProperty("javax.net.ssl.trustStore", "src/main/resources/client.jks");
-//        System.setProperty("javax.net.ssl.trustStorePassword", "123456");
-//        EurekaJerseyClientBuilder builder = new EurekaJerseyClientBuilder();
-//        builder.withClientName("client");
-//        builder.withSystemSSLConfiguration();
-//        builder.withMaxTotalConnections(10);
-//        builder.withMaxConnectionsPerHost(10);
-//        args.setEurekaJerseyClient(builder.build());
-//
-//        RestTemplateBuilder builder1 = new RestTemplateBuilder();
-//        builder1.setSslBundle(S)
-//        return args;
-//    }
-
 }
