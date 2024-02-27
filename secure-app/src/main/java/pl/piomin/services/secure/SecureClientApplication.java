a8f73c3729ba025c6d6e63f27f6f3c95c428a00a
piomin <piotr.minkowski@gmail.com>
2024-02-27 23:50:43 +0100

    fix ssl/tls settings

--- secure-app/src/main/java/pl/piomin/services/secure/SecureClientApplication.java
+++ secure-app/src/main/java/pl/piomin/services/secure/SecureClientApplication.java
@@ -1,13 +1,7 @@
 package pl.piomin.services.secure;
 
-import java.security.NoSuchAlgorithmException;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
-
-import com.netflix.discovery.DiscoveryClient;
-import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl.EurekaJerseyClientBuilder;
 
 @SpringBootApplication
 public class SecureClientApplication {
@@ -16,20 +10,24 @@ public class SecureClientApplication {
         new SpringApplicationBuilder(SecureClientApplication.class).run(args);
     }
 
-    @Bean
-    public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws NoSuchAlgorithmException {
-        DiscoveryClient.DiscoveryClientOptionalArgs args = new DiscoveryClient.DiscoveryClientOptionalArgs();
-        System.setProperty("javax.net.ssl.keyStore", "src/main/resources/client.jks");
-        System.setProperty("javax.net.ssl.keyStorePassword", "123456");
-        System.setProperty("javax.net.ssl.trustStore", "src/main/resources/client.jks");
-        System.setProperty("javax.net.ssl.trustStorePassword", "123456");
-        EurekaJerseyClientBuilder builder = new EurekaJerseyClientBuilder();
-        builder.withClientName("client");
-        builder.withSystemSSLConfiguration();
-        builder.withMaxTotalConnections(10);
-        builder.withMaxConnectionsPerHost(10);
-        args.setEurekaJerseyClient(builder.build());
-        return args;
-    }
+//    @Bean
+//    public RestTemplateDiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws NoSuchAlgorithmException {
+//        RestTemplateDiscoveryClientOptionalArgs args = new RestTemplateDiscoveryClientOptionalArgs();
+//
+//        System.setProperty("javax.net.ssl.keyStore", "src/main/resources/client.jks");
+//        System.setProperty("javax.net.ssl.keyStorePassword", "123456");
+//        System.setProperty("javax.net.ssl.trustStore", "src/main/resources/client.jks");
+//        System.setProperty("javax.net.ssl.trustStorePassword", "123456");
+//        EurekaJerseyClientBuilder builder = new EurekaJerseyClientBuilder();
+//        builder.withClientName("client");
+//        builder.withSystemSSLConfiguration();
+//        builder.withMaxTotalConnections(10);
+//        builder.withMaxConnectionsPerHost(10);
+//        args.setEurekaJerseyClient(builder.build());
+//
+//        RestTemplateBuilder builder1 = new RestTemplateBuilder();
+//        builder1.setSslBundle(S)
+//        return args;
+//    }
 
 }
